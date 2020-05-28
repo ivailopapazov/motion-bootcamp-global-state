29a796625c8313fd8036c377d5a21e18279b4c6a
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2020-05-28 15:32:54 +0300

    Add local storage state persistance

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,12 +1,31 @@
 import { createStore, combineReducers } from './dedux';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer);
+const saveState = state => {
+    let serializedState = JSON.stringify(state);
+
+    localStorage.setItem('state', serializedState);
+};
+
+const loadState = () => {
+    let serializedState = localStorage.getItem('state');
+
+    if (serializedState) {
+        return JSON.parse(serializedState);
+    }
+}; 
+
+const store = createStore(rootReducer, loadState());
+
+store.subscribe(state => {
+    // TODO: Add throttle
+    saveState(state);
+});
 
 // Logger
 store.subscribe(state => {
     console.log('State Changed:');
     console.log(state);
-})
+});
 
 export default store;
